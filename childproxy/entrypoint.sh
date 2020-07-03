f2d3bfe2bf0b10a617c581c3dd36be62d5081fbf
hsmtkk <hsmtkk@gmail.com>
2020-07-04 06:47:15 +0900

    configuring docker env

---
+++ childproxy/entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+squid=/usr/local/squid
+
+chown -R nobody:nobody ${squid}/var/logs
+${squid}/libexec/security_file_certgen -c -s ${squid}/var/cache/squid/ssl_db -M 4MB
+chown -R nobody:nobody ${squid}/var/cache/squid
+
+${squid}/sbin/squid
+tail -f ${squid}/var/logs/access.log ${squid}/var/logs/cache.log
+
