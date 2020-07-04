719ef9afab2f87b9247faf00df89058ecadc3dda
hsmtkk <hsmtkk@gmail.com>
2020-07-05 07:38:36 +0900

    update to use squid latest version

--- parentproxy/entrypoint.sh
+++ parentproxy/entrypoint.sh
@@ -5,10 +5,10 @@ echo '/tmp/core.%h.%e.%t' > /proc/sys/kernel/core_pattern
 squid=/usr/local/squid
 
 touch ${squid}/var/logs/access.log ${squid}/var/logs/cache.log
-chown -R nobody:nobody ${squid}/var/logs
+chown -R nobody:nogroup ${squid}/var/logs
 
 ${squid}/libexec/security_file_certgen -c -s ${squid}/var/cache/squid/ssl_db -M 4MB
-chown -R nobody:nobody ${squid}/var/cache/squid
+chown -R nobody:nogroup ${squid}/var/cache/squid
 
 ${squid}/sbin/squid
 tail -F ${squid}/var/logs/access.log ${squid}/var/logs/cache.log
